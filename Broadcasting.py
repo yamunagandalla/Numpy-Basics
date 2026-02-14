16018c3cb9c4be2496e17a64aebd3b4fcd8e222d
YAMUNA GANDALLA-N220648 <n220648@rguktn.ac.in>
2026-02-14 18:35:27 +0000

    updates

--- Broadcasting.py
+++ Broadcasting.py
@@ -2,4 +2,15 @@ import numpy as np
 a=np.array([[1,2,3],[4,5,6]])
 print(a)
 print(a.shape)
-print(a+5)
+print(a+5) 
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a)
+print(a.shape)
+b=np.array([[1],[2],[3]])
+print(b)
+print(b.shape)
+c=a+b
+print(c)
+print(c.shape)
+
