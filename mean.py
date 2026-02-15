8ce0431b862ec4fcdf9c8c22985edce99edb867a
YAMUNA GANDALLA-N220648 <n220648@rguktn.ac.in>
2026-02-15 12:03:20 +0000

    adding my work on mean , median and standard deviation

---
+++ mean.py
@@ -0,0 +1,9 @@
+import numpy as np 
+arr=np.array([1,2,3,4,5,6])
+print("the array is :",arr)
+#mean
+print("Mean:",np.mean(arr))
+#median
+print("Median:",np.median(arr))
+#standard deviation
+print("Standard Deviation:",np.std(arr))
